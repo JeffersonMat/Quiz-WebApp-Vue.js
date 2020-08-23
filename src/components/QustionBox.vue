3bd44d9706475da66825aa28bc22f80c5f13673e
JeffersonMat <jeff_08062011@hotmail.com>
2020-08-23 16:35:55 +1200

    add selectedAnswer and added class properties in style

--- src/components/QustionBox.vue
+++ src/components/QustionBox.vue
@@ -8,6 +8,8 @@
       <b-list-group>
         <b-list-group-item v-for="(answer, index) in answers"
          :key="index"
+         @click="selectAnswer(index)"
+         :class ="[selectedIndex ===  index ? 'selected'  : '']"
          >
           {{answer}}
         </b-list-group-item>
@@ -25,14 +27,57 @@ export default {
     currentQuestion: Object,
     next: Function,
   },
+  data(){
+   return{ selectedIndex:null,
+   }
+  },
 
   computed: {
     answers() {
       let answers = [...this.currentQuestion.incorrect_answers];
       answers.push(this.currentQuestion.correct_answer);
       return answers;
-    },
-  },
-};
+    }
+   },
+  methods: {
+    selectAnswer(index){
+      this.selectedIndex = index
+
+    }
+  } 
+}
 </script>
+
+<style scoped>
+
+.list-group{
+  
+  margin-bottom:15px;
+  }
+
+.list-group-item:hover{
+
+background: #eee;
+cursor: pointer;
+}
+
+
+  .btn{
+    margin: 0 5px;
+  }
+  .selected {
+    background: lightblue
+  }
+
+  .correct{
+  background: green
+  }
+
+  .incorrect{
+    
+    background: red
+    }
+</style>>
+
+
 
